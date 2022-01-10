a39513a40e05a504ab68cc07d97d496294161cff
Ahmad Opeyemi Busari <ahmadinjo@users.noreply.github.com>
2022-01-10 12:39:39 +0100

    Refactored cors again to include other headers

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ dotenv.config();
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
